0eab70accd1ed6a9433671b8fc05f44d92489819
Nejah129 <nejahyengui999@gmail.com>
2022-08-18 16:49:37 +0100

    test

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "mydatabase",
+    "password": "mydatabase",
+    "database": "mydatabase",
+    "host": "mydatabase.c0pasen15phd.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
