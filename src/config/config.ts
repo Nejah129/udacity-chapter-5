fe30a9fbb3c1a7c9e4d490f2f21b464560947157
Nejah129 <nejahyengui999@gmail.com>
2022-08-20 20:19:45 +0100

    building script with elastickbeanstalk

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,18 +5,18 @@ export const config = {
     "database": "mydatabase",
     "host": "mydatabase.c0pasen15phd.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "s3test1usergroup"
   },
   "jwt": {
     "secret": " "
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+    "username": "mydatabase",
+    "password": "mydatabase",
+    "database": "mydatabase",
+    "host": "mydatabase.c0pasen15phd.us-east-1.rds.amazonaws.com",
     "dialect": "postgres"
   }
 }
